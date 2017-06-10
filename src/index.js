bd5a50da40f5030051faa827dc4a4c5da1f005c6
prosdev <prosdevlab@gmail.com>
2017-06-09 23:41:50 -0700

    Syntax highlight for example

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import DocLab from './docs/DocLab';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import '../node_modules/highlight.js/styles/ocean.css';
 
 ReactDOM.render(
     <DocLab />, document.getElementById('root'));
