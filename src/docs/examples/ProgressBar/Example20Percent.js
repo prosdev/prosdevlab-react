31529730ce582e9963d14faec68cec9f66103376
prosdev <prosdevlab@gmail.com>
2017-06-10 01:05:46 -0700

    ProgressBar component

---
+++ src/docs/examples/ProgressBar/Example20Percent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ProgressBar from 'prosdevlab-react/ProgressBar/ProgressBar';
+
+/** 20% progress */
+export default function Example20Percent() {
+    return <ProgressBar percent={20} width={200}/>
+}
